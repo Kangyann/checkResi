ed39545a2aacccdaabf003003f8c78b2a7f42f8f
Kangyann <kangyann727@gmail.com>
2024-05-23 19:47:15 +0700

    build main pages

--- src/core/components/organisms/Details/index.jsx
+++ src/core/components/organisms/Details/index.jsx
@@ -1,39 +1,39 @@
 const Details = ({ ...props }) => {
   return (
     <>
-        <p className="text-success text-center my-3">{props.message}</p>
-        <div className="flex justify-between">
-          <div className="">
-            <p>Penerima : {props.data.detail.receiver}</p>
-            <p>Pengirim : {props.data.summary.courier}</p>
-          </div>
-          <div className="">
-            <p>{props.data.summary.awb}</p>
-            <p>{props.data.summary.status}</p>
-          </div>
+      <p className="text-success text-center my-3">{props.message}</p>
+      <div className="flex justify-between lg:flex-row md:flex-row flex-col">
+        <div className="">
+          <p>Penerima : {props.data.detail.receiver}</p>
+          <p>Pengirim : {props.data.summary.courier}</p>
         </div>
-        <div className="mt-3 overflow-auto h-96">
-          <table className="table lg:table-md md:table-md table-sm">
-            <thead>
-              <tr>
-                <th>Tanggal</th>
-                <th>Deskripsi</th>
-                {props.data.history[0].location !== "" && <th>Lokasi</th>}
+        <div className="">
+          <p>No Resi : {props.data.summary.awb}</p>
+          <p>Status : {props.data.summary.status}</p>
+        </div>
+      </div>
+      <hr className="my-3 border-[rgb(255,255,255,.25)]" />
+      <div className="mt-3 overflow-auto h-96">
+        <table className="table lg:table-md md:table-md table-sm">
+          <thead>
+            <tr>
+              <th>Tanggal</th>
+              <th>Deskripsi</th>
+              {props.data.history[0].location !== "" && <th>Lokasi</th>}
+            </tr>
+          </thead>
+          <tbody>
+            {props.data.history.map((val, index) => (
+              <tr key={`history-${index}`}>
+                <td>{val.date}</td>
+                <td>{val.desc}</td>
+                <td>{val.location && val.location}</td>
               </tr>
-            </thead>
-            <tbody>
-              {props.data.history.map((val, index) => (
-                <>
-                  <tr key={`history-${index}`}>
-                    <td>{val.date}</td>
-                    <td>{val.desc}</td>
-                    <td>{val.location && val.location}</td>
-                  </tr>
-                </>
-              ))}
-            </tbody>
-          </table>
-        </div>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <hr className="border-[rgb(255,255,255,.25)]" />
     </>
   );
 };
--- src/core/components/organisms/Footer/index.jsx
+++ src/core/components/organisms/Footer/index.jsx
@@ -2,10 +2,16 @@ const FooterComponent = () => {
   return (
     <>
       <div className="w-full text-center my-4">
-        <p>RETRA &copy; by Kangyann. 2024 All Right Reserved.</p>
+        <p>
+          <b>RETRA</b> &copy; by
+          <a href="https://instagram.com/kangyann.tsx" className="link-hover">
+            Kangyann.
+          </a>
+          2024 All Right Reserved.
+        </p>
       </div>
     </>
   );
 };
 
-export default FooterComponent
+export default FooterComponent;
