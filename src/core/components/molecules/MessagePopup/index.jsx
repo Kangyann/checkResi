00c25c546c942bdc8b59190eed37250660a1255a
Kangyann <kangyann727@gmail.com>
2024-05-25 22:26:49 +0700

    fixing ; on messagepopup

--- src/core/components/molecules/MessagePopup/index.jsx
+++ src/core/components/molecules/MessagePopup/index.jsx
@@ -1,7 +1,7 @@
 const MessagePopupComponent = ({ text }) => {
   return (
     <>
-      <p className="text-center text-error mt-3">{text}</p>;
+      <p className="text-center text-error mt-3">{text}</p>
     </>
   );
 };
